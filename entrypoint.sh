bf6f4c7b1e521a2dae991a6ef3c378a4eb10e6a0
Carlos Rocha <carlos@rocha.cc>
2018-02-15 10:34:26 -0500

    trying to get parameters in json format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-aws ssm get-parameters --names test_param test_param2 "${ENV,,}_param" --no-with-decryption --region $DEFAULT_REGION >> /usr/local/apache2/htdocs/secrets.txt
+aws ssm get-parameters --names test_param test_param2 $ENV_param --no-with-decryption --region $DEFAULT_REGION >> /usr/local/apache2/htdocs/secrets.txt
 echo $ENV >> /usr/local/apache2/htdocs/env.txt
 
 
