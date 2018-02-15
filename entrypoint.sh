d3612fe53a5f94b636f3beeb1847f92821a95e8b
Carlos Rocha <carlos@rocha.cc>
2018-02-15 11:38:32 -0500

    trying to get all parameters under hierarchy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 aws ssm get-parameters --names test_param test_param2 ${ENV}_param --no-with-decryption --region $DEFAULT_REGION >> /usr/local/apache2/htdocs/secrets.txt
 
-aws ssm get-parameters-by-path --path /$ENV --recursive --no-with-decryption --region $DEFAULT_REGION >> /usr/local/apache2/htdocs/secrets_bypath.txt
+aws ssm get-parameters-by-path --path /$ENV --recursive --with-decryption --region $DEFAULT_REGION >> /usr/local/apache2/htdocs/secrets_bypath.txt
 
 echo $ENV >> /usr/local/apache2/htdocs/env.txt
 
